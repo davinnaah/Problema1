f14e1564f8035aacfa3989bab258e316e3847757
User <User@DESKTOP-PQKU9A0>
2022-02-07 09:47:45 +0200

    Add project files.

---
+++ Problema1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Problema1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int = x;
+            int suma = Recursivitate.Suma(x);
+            Console.WriteLine($"Suma({x})= {suma}");
+        }
+    }
+}
---
+++ Problema1/Recursivitate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema1
+{
+    internal class Recursivitate
+    {
+        public static int Suma(int x)
+        {
+
+        }
+    }
+}
